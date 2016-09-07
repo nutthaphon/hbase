677e2661392e00e1c918d6f23cb8ccd20b737d4d
Nutthaphon Su <nutthaphon@gmail.com>
2016-09-07 20:50:36 +0700

    Start Thrift Service

--- startall.sh
+++ startall.sh
@@ -1,4 +1,5 @@
 service ssh start
 su - hadoop -c "hadoop-2.7.3/sbin/start-dfs.sh"
 su - hbase -c "hbase-1.2.2/bin/start-hbase.sh"
+su - hbase -c "hbase-1.2.2/bin/hbase-daemon.sh start thrift"
 tail -f ~hbase/hbase-1.2.2/logs/hbase-hbase-master*
